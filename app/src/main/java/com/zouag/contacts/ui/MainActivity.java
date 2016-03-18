639176c69088f5f5b60f19b1a491244cf5528280
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-18 22:43:33 +0000

    Setup an empty view to be displayed in case there were no available contacts.

--- app/src/main/java/com/zouag/contacts/ui/MainActivity.java
+++ app/src/main/java/com/zouag/contacts/ui/MainActivity.java
@@ -2,24 +2,63 @@ package com.zouag.contacts.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.zouag.contacts.R;
+import com.zouag.contacts.adapters.ContactsAdapter;
+import com.zouag.contacts.models.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * The main contacts' ListView.
+     */
     @Bind(R.id.contactsList)
     ListView contactsListView;
+
+    /**
+     * The TextView to be displayed in case there were no stored contacts.
+     */
+    @Bind(R.id.noContactsEmptyView)
+    TextView emptyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        // Get the list of contacts
+        List<Contact> contacts = getContacts();
+
+        /* Set the visibility of the empty view & the contactsListView
+        according to the contacts' state */
+        emptyView.setVisibility(contacts.size() == 0 ? View.VISIBLE : View.INVISIBLE);
+        contactsListView.setVisibility(contacts.size() == 0 ?
+                View.INVISIBLE : View.VISIBLE);
+
+        // Setup the adapter & the ListView
+        ContactsAdapter adapter = new ContactsAdapter(this, contacts);
+        contactsListView.setAdapter(adapter);
+    }
+
+    /**
+     * @return the full list of contacts.
+     */
+    private List<Contact> getContacts() {
+        return new ArrayList<>();
     }
 }
