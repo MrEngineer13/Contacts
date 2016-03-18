45748529c1a1da0a86fe8dd84667ddfc99e8b0e1
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-18 22:26:34 +0000

    Add the 'ContactsAdapter' class & the XML layout of a contact.

---
+++ app/src/main/java/com/zouag/contacts/adapters/ContactsAdapter.java
@@ -0,0 +1,53 @@
+package com.zouag.contacts.adapters;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.zouag.contacts.R;
+import com.zouag.contacts.models.Contact;
+
+import java.util.List;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 18/03/2016.
+ */
+public class ContactsAdapter extends ArrayAdapter<Contact> {
+
+    private Context context;
+    private List<Contact> contacts;
+
+    public ContactsAdapter(Context context, List<Contact> contacts) {
+        super(context, -1, contacts);
+        this.context = context;
+        this.contacts = contacts;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = ((AppCompatActivity) context).getLayoutInflater();
+            convertView = inflater.inflate(R.layout.contacts_row, parent, false);
+            // Setup the ViewHolder
+            viewHolder = new ViewHolder();
+            viewHolder.nameText = (TextView) convertView.findViewById(R.id.nameText);
+
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView nameText;
+    }
+}
--- app/src/main/java/com/zouag/contacts/ui/MainActivity.java
+++ app/src/main/java/com/zouag/contacts/ui/MainActivity.java
@@ -2,14 +2,24 @@ package com.zouag.contacts.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.zouag.contacts.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.contactsList)
+    ListView contactsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
     }
 }
