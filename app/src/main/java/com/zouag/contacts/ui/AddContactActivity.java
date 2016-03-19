02631fe477d82fca7122cd29bd2c4cd7a51a5462
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-19 19:02:40 +0000

    Save the image taken from camera to local disk & its path.

--- app/src/main/java/com/zouag/contacts/ui/AddContactActivity.java
+++ app/src/main/java/com/zouag/contacts/ui/AddContactActivity.java
@@ -2,18 +2,29 @@ package com.zouag.contacts.ui;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.zouag.contacts.R;
 import com.zouag.contacts.adapters.DatabaseAdapter;
 import com.zouag.contacts.models.Contact;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -21,6 +32,9 @@ import butterknife.ButterKnife;
 public class AddContactActivity extends AppCompatActivity {
 
     private static final int REQUEST_OPEN_GALLERY = 10;
+    private static final int REQUEST_OPEN_CAMERA = 11;
+
+    private String current_img_path = "";
 
     @Bind(R.id.contactName)
     EditText contactName;
@@ -52,10 +66,80 @@ public class AddContactActivity extends AppCompatActivity {
                 switch (resultCode) {
                     case RESULT_OK:
                         Uri imageData = data.getData();
+                        Log.i("CONTACTS", getRealPathFromURI(imageData));
                         contactImage.setImageURI(imageData);
                         break;
                 }
+                break;
+            case REQUEST_OPEN_CAMERA:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        Bundle extras = data.getExtras();
+                        Bitmap image = (Bitmap) extras.get("data");
+                        saveImageToDisk(image);
+                        contactImage.setImageBitmap(image);
+                        break;
+                }
+                break;
         }
+    }
+
+    private void saveImageToDisk(Bitmap image) {
+        // 1. Get the external storage directory
+        String appName = getString(R.string.app_name);
+        File mediaStorageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                appName);
+
+        // 2. Create our subdirectory
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.e("CONTACTS", "Failed to create directory.");
+            }
+        }
+
+        // 3. Create a file name
+        // 4. Create the file
+        Date now = new Date();
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US).format(now);
+        String path = mediaStorageDir.getPath() + File.separator;
+
+        // Save the path of the image
+        current_img_path = path + "IMG_" + timestamp + ".jpg";
+        Log.i("CONTACTS", "Camera path: " + current_img_path);
+        File mediaFile = new File(current_img_path);
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(mediaFile);
+            image.compress(Bitmap.CompressFormat.PNG, 100, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.i("CONTACTS", "Image successfully saved.");
+    }
+
+    private String getRealPathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
     }
 
     /**
@@ -95,7 +179,7 @@ public class AddContactActivity extends AppCompatActivity {
      * @param view the button.
      */
     public void onImageClicked(View view) {
-        CharSequence options[] = new CharSequence[] {"Gallery", "Camera"};
+        CharSequence options[] = new CharSequence[]{"Gallery", "Camera"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Choose image from ...");
@@ -107,7 +191,8 @@ public class AddContactActivity extends AppCompatActivity {
                     startActivityForResult(gallery, REQUEST_OPEN_GALLERY);
                     break;
                 case 1:
-                    Toast.makeText(this, "Camera", Toast.LENGTH_SHORT).show();
+                    Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera, REQUEST_OPEN_CAMERA);
                     break;
             }
         });
@@ -138,6 +223,7 @@ public class AddContactActivity extends AppCompatActivity {
                     .phoneNumber(phoneNumber)
                     .email(emailStatus ? email : "")
                     .address(addressStatus ? address : "")
+                    .imgPath(current_img_path)
                     .createContact();
         } else if (!nameStatus) {
             // Invalid contact name.
