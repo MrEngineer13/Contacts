452b20597edce012b8829308e1bf3ff4103a2ba7
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-20 18:21:54 +0000

    Add the 'ViewContactActivity'

---
+++ app/src/main/java/com/zouag/contacts/ui/ViewContactActivity.java
@@ -0,0 +1,15 @@
+package com.zouag.contacts.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zouag.contacts.R;
+
+public class ViewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_contact);
+    }
+}
