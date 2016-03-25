98ef446e7e0984ab757b55332b3c690a586e6440
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-25 09:43:26 +0000

    Use a PreferenceFragment instead of a PreferenceActivity.

---
+++ app/src/main/java/com/zouag/contacts/ui/SettingsFragment.java
@@ -0,0 +1,18 @@
+package com.zouag.contacts.ui;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.zouag.contacts.R;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 25/03/2016.
+ */
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.settings);
+    }
+}
